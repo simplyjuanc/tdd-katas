97e0eae05606fd1c82bff3d50c47f2efcfaa5e75
Juan Vasquez <37302562+simplyjuanc@users.noreply.github.com>
2025-04-10 19:51:07 +0100

    rename package

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.20"
 }
 
-group = "org.example"
+group = "org.money"
 version = "1.0-SNAPSHOT"
 
 repositories {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,6 @@
-package org.example
+package org.money
 
 fun main() {
     println("Hello World!")
 }
 
-data class Dollar(val amount: Int) {
-    fun times(multiplier: Int) = Dollar(this.amount * multiplier)
-}
