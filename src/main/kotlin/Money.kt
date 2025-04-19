d11035aaf4c621bd92cbc2761b768ed2688c66c0
Juan Vasquez <37302562+simplyjuanc@users.noreply.github.com>
2025-04-19 15:44:27 +0100

    add expression and bank objects

--- src/main/kotlin/Money.kt
+++ src/main/kotlin/Money.kt
@@ -3,7 +3,7 @@ package org.money
 open class Money(
     open val amount: Int,
     open val currency: String,
-) {
+): Expression {
     fun currency() = currency
 
     override fun equals(other: Any?): Boolean =
@@ -13,17 +13,20 @@ open class Money(
 
     override fun hashCode(): Int = amount
 
-    fun times(multiplier: Int): Money {
-        return Money(amount * multiplier, currency())
-    }
+    fun times(multiplier: Int): Money = Money(amount * multiplier, currency())
+
+    fun plus(addend: Money): Money = Money(amount + addend.amount, currency())
 
     companion object {
-        fun dollar(amount:Int) = Dollar(amount, "USD")
-        fun franc(amount:Int) = Franc(amount, "CHF")
+        fun dollar(amount:Int) = Money(amount, "USD")
+        fun franc(amount:Int) = Money(amount, "CHF")
     }
 }
 
-class Dollar(amount: Int, currency: String) : Money(amount, currency)
+interface Expression
 
-class Franc(amount: Int, currency: String): Money(amount, currency)
-
+class Bank {
+    fun reduce(sum: Expression, currency: String): Expression {
+        return Money.dollar(10)
+    }
+}
--- src/test/kotlin/MoneyTest.kt
+++ src/test/kotlin/MoneyTest.kt
@@ -1,38 +1,10 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
-import org.money.Franc
+import org.money.Bank
 import org.money.Money
 
 class MoneyTest {
-    @Test
-    fun `should multiply dollars correctly`() {
-        val dollar = Money.dollar(5)
-
-        assertEquals(Money.dollar(10), dollar.times(2))
-        assertEquals(Money.dollar(15), dollar.times(3))
-    }
-
-    @Test
-    fun `should assert equality between dollars`() {
-        assertEquals(Money.dollar(5), Money.dollar(5))
-        assertNotEquals(Money.dollar(7), Money.dollar(6))
-    }
-
-    @Test
-    fun `should multiply francs correctly`() {
-        val franc = Money.franc(5)
-
-        assertEquals(Money.franc(10), franc.times(2))
-        assertEquals(Money.franc(15), franc.times(3))
-    }
-
-    @Test
-    fun `should assert equality between francs`() {
-        assertEquals(Money.franc(5), Money.franc(5))
-        assertNotEquals(Money.franc(7), Money.franc(6))
-    }
-
     @Test
     fun `should assert equality for the same currencies and values`() {
         assertEquals(Money.dollar(5), Money.dollar(5))
@@ -53,7 +25,31 @@ class MoneyTest {
     }
 
     @Test
-    fun `should test value equality not class equality`() {
-        assertEquals(Franc(5, "CHF"), Money.franc(5))
+    fun `should multiply dollars correctly`() {
+        val dollar = Money.dollar(5)
+
+        assertEquals(Money.dollar(10), dollar.times(2))
+        assertEquals(Money.dollar(15), dollar.times(3))
+    }
+
+    @Test
+    fun `should multiply francs correctly`() {
+        val franc = Money.franc(5)
+
+        assertEquals(Money.franc(10), franc.times(2))
+        assertEquals(Money.franc(15), franc.times(3))
+    }
+
+    @Test
+    fun `5 plus 5 should add 10`() {
+        val sum = Money.dollar(5).plus(Money.dollar(5))
+        assertEquals(Money.dollar(10), sum)
+    }
+
+    @Test
+    fun `should add multi-currency`() {
+        val sum = Money.dollar(5).plus(Money.franc(5))
+        val reduced = Bank().reduce(sum, "USD")
+        assertEquals(Money.dollar(10), reduced)
     }
 }
